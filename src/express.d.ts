241c9f1de940b110dd28040a8daebad1659dc58f
Male Michael <michael.male@andela.com>
2019-04-26 13:44:32 +0300

    add location to the request object

--- src/express.d.ts
+++ src/express.d.ts
@@ -1,7 +1,9 @@
 import {Request} from 'express';
+import { Location } from './models/population.model';
 import { User } from './models/user.model';
 
 interface IRequest extends Request {
+  location?: Location;
   user?: User;
   email?: string
 }
